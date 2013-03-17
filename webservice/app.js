b793d63fd1ab353cf31292404de9bfa9f30670e5
opensas <opensas@gmail.com>
2013-03-17 15:16:04 -0300

    remove test controller and add mock controller

--- webservice/app.js
+++ webservice/app.js
@@ -7,20 +7,27 @@ var server = restify.createServer({
 });
 
 var controllers = require('./controllers');
-console.log(controllers);
 
 server.use(restify.acceptParser(server.acceptable));
 server.use(restify.queryParser());
 server.use(restify.bodyParser({ mapParams: false }));
 
 server.get('/ping', controllers.ping.ping);
-server.get('/test', controllers.test.test);
 
 server.get('/users', controllers.users.list);
 server.get('/users/:id', controllers.users.show);
 
 server.get('/types', controllers.ideasTypes.list);
 server.get('/types/:id', controllers.ideasTypes.show);
+
+var mock = require('./controllers/mock');
+
+server.get('/mock/users', mock.users.list);
+server.get('/mock/users/:id', mock.users.show);
+
+// server.get(/\/docs\/public\/?.*/, restify.serveStatic({
+//   directory: './public'
+// }));
 
 server.listen(1337, function() {
   console.log('%s listening at %s', server.name, server.url);
