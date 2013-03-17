59ed71dab98461a2ae769f529208ee6e614bf49d
Alan Reid <mail@alanreid.com.ar>
2013-03-17 19:32:55 -0300

    Updated models

--- webservice/controllers/ideasTypes.js
+++ webservice/controllers/ideasTypes.js
@@ -1,18 +1,26 @@
-var IdeaType = require('../db.js').models.IdeaType;
 
-module.exports = {
+var IdeasTypesController = {
 
   list: function(req, res, next) {
+
+    var IdeaType = IdeasTypesController.models.IdeaType;
+
     IdeaType.findAll().success(function(ideaTypes) {
       res.send(ideaTypes);
     });
+
   },
 
   show: function(req, res, next) {
-    console.log(req.params);
+
+    var IdeaType = IdeasTypesController.models.IdeaType;
+
     IdeaType.find(parseInt(req.params.id, 10)).success(function(ideaType) {
       res.send(ideaType);
     });
+
   }
 
 };
+
+module.exports = IdeasTypesController;
--- webservice/controllers/users.js
+++ webservice/controllers/users.js
@@ -1,17 +1,25 @@
-var User = require('../db.js').models.User;
 
-module.exports = {
+
+var UsersController = {
 
   list: function(req, res, next) {
+
+    var User = UsersController.models.User;
+
     User.findAll().success(function(users) {
       res.send(users);
     });
   },
 
   show: function(req, res, next) {
+
+    var User = UsersController.models.User;
+
     User.find(parseInt(req.params.id, 10)).success(function(user) {
       res.send(user);
     });
   }
 
 };
+
+module.exports = UsersController;
