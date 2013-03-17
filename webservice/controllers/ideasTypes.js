3f686b9537808067bd4fc8bf6123789567cf52e6
opensas <opensas@gmail.com>
2013-03-17 15:12:58 -0300

    move db.js file out of the models folder

--- webservice/controllers/ideasTypes.js
+++ webservice/controllers/ideasTypes.js
@@ -1,15 +1,18 @@
-var IdeaType = require('../models/db.js').models.IdeaType;
+var IdeaType = require('../db.js').models.IdeaType;
 
-exports.list = function(req, res, next) {
-  IdeaType.findAll().success(function(ideaTypes) {
-    res.send(ideaTypes);
-  });
+module.exports = {
+
+  list: function(req, res, next) {
+    IdeaType.findAll().success(function(ideaTypes) {
+      res.send(ideaTypes);
+    });
+  },
+
+  show: function(req, res, next) {
+    console.log(req.params);
+    IdeaType.find(parseInt(req.params.id, 10)).success(function(ideaType) {
+      res.send(ideaType);
+    });
+  }
+
 };
-
-exports.show = function(req, res, next) {
-  console.log(req.params);
-  IdeaType.find(parseInt(req.params.id, 10)).success(function(ideaType) {
-    res.send(ideaType);
-  });
-};
-
